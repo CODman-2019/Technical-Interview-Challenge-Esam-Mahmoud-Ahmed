cad551ad9eb20f2b5bf9bf6c24dfd8710d4f4d63
Esam Mahmoud-Ahmed <55792105+CODman-2019@users.noreply.github.com>
2024-09-12 00:30:10 -0300

    created player movement, jump and attack

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] private float          cooldownTimer;
+    //[SerializeField] private AnimationClip  attackAnim;
+    [SerializeField] private Animator       animor;
+
+    private void Start()
+    {
+        animor = GetComponent<Animator>();
+        //anim = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Attack");
+            animor.SetTrigger("Attack");
+            StartCoroutine(Resetanim());
+        }
+
+    }
+
+    IEnumerator Resetanim()
+    {
+        yield return new WaitForSeconds(cooldownTimer);
+        animor.ResetTrigger("Attack");
+        Debug.Log("reset complete");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float      player_Speed;
+    [SerializeField] private float      groundDistance;
+    [SerializeField] private float      jumpForce;
+    [SerializeField] private float      groundCheckRaidius;
+    [SerializeField] private LayerMask  groundMask;
+
+    private Rigidbody                   rb;
+    private Vector3                     originDistance;
+    private float                       player_ForwardMovement;
+    private float                       player_SideMovement;
+    private bool                        check_Grounded;
+
+    private RaycastHit groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player_ForwardMovement = 0;
+        player_SideMovement = 0;
+        check_Grounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //MOVEMENT
+        player_ForwardMovement = Input.GetAxisRaw("Vertical") * player_Speed;
+        player_SideMovement = Input.GetAxisRaw("Horizontal") * player_Speed;
+
+        rb.velocity = new Vector3(player_SideMovement, rb.velocity.y, player_ForwardMovement);
+
+        //ground check set up
+        originDistance = new Vector3(transform.position.x, transform.position.y - groundDistance, transform.position.z);
+        //Debug.Log(originDistance);
+        GroundCheck();
+
+        if (Input.GetButton("Jump"))
+        {
+            if(check_Grounded)
+            {
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+                check_Grounded = false;
+            }
+        }
+
+        
+    }
+
+    void GroundCheck()
+    {
+        if (!Physics.SphereCast(originDistance, groundCheckRaidius, Vector3.down, out groundCheck, groundMask))
+        {
+            //Debug.Log("hiting the ground");
+            if(!check_Grounded)
+            check_Grounded = true;
+        }
+        else 
+        {
+            //Debug.Log("not hitting the ground");
+            if (check_Grounded)
+                check_Grounded = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.yellow; 
+        Gizmos.DrawSphere(originDistance, groundCheckRaidius);
+    }
+}
